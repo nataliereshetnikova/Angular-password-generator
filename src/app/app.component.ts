f0e2d3e62b0b985de2e86eb2b95e4c7fa662e447
nataliereshetnikova <ab1748@gmail.com>
2020-10-27 11:57:20 -0400

    add event listeners

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,7 +8,36 @@ import { element } from 'protractor';
 })
 export class AppComponent {
   password = '';
+  useLetters = false;
+  useNumbers = false;
+  useSymbols = false;
+  length = 8;
   onGenerate(){
+    const numbers='1234567890';
+    const letters='abcdefghijklmnopqrstuvwxyz';
+    const symbols = '!@#$%^&*()';
+    let validChars='';
+    if(this.useLetters){
+      validChars+=letters;
+    }
+    if(this.useNumbers){
+      validChars+=numbers;
+    }
+    if(this.useSymbols){
+      validChars+=symbols;
+    }
     this.password = 'generated password';
+  }
+  onChangeUseLetters(){
+    this.useLetters= !this.useLetters;
+  }
+  onChangeUseNumbers(){
+    this.useNumbers = !this.useNumbers;
+  }
+  onChangeUseSymbols(){
+    this.useSymbols = !this.useSymbols;
+  }
+  onChangeLength(e){
+    this.length = e.target.value;
   }
 }
